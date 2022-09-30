fb2f56821a4c06a60a3f640515b79590f4636e41
narumincho <16481886+narumincho@users.noreply.github.com>
2022-09-30 10:19:41 +0900

    始まってからの秒数の表示を追加

--- main.ts
+++ main.ts
@@ -2,5 +2,7 @@ const start = new Date();
 
 setInterval(() => {
   const now = new Date();
-  console.log((now.getTime() - start.getTime()) / 1000);
+  console.log(
+    "始まってからの秒数: " + (now.getTime() - start.getTime()) / 1000
+  );
 }, 1000);
